db8d9ca2bf46e6e46729e5963b773ce08deee93f
Terry Peppers <peppers@gmail.com>
2013-10-18 07:44:25 -0500

    BOD commit. Missed one last night.

---
+++ callbacks/9_callbacks.js
@@ -0,0 +1,16 @@
+
+var arr = ["foo", "bar", "baz"];
+
+arr.forEach(function(x) { console.log(x.toUpperCase()); })
+
+// var arr = [1,2,3,4,5];
+
+// function odd_or_even(x) {
+// 	if (x % 2 > 0) {
+// 		console.log(x + " is odd!");
+// 	} else {
+// 		console.log(x + " is even!");
+// 	}
+// }
+
+// arr.forEach(odd_or_even(x));
